910953d075e8462dd7b3bfd554e4c187e9b09bac
ObNitram <mart1.guillemot@gmail.com>
2023-12-01 12:57:36 +0800

    init

---
+++ lib/ui/drawable_zone.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class DrawableZone extends StatefulWidget {
+  const DrawableZone({super.key});
+
+  @override
+  State<DrawableZone> createState() => _DrawableZoneState();
+}
+
+class _DrawableZoneState extends State<DrawableZone> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
