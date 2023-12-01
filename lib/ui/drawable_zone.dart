35829e1af780c67a5a750771c43b21f3c2fad1eb
ObNitram <mart1.guillemot@gmail.com>
2023-12-01 15:43:53 +0800

    Add DrawableCircle for drawing circle

--- lib/ui/drawable_zone.dart
+++ lib/ui/drawable_zone.dart
@@ -1,5 +1,6 @@
 import "package:flutter/widgets.dart";
 import "package:obni_draw/ui/drawable/drawable.dart";
+import "package:obni_draw/ui/drawable/drawable_circle.dart";
 import "package:obni_draw/ui/drawable/drawable_rect.dart";
 
 class DrawableZone extends StatefulWidget {
@@ -10,12 +11,15 @@ class DrawableZone extends StatefulWidget {
 }
 
 class _DrawableZoneState extends State<DrawableZone> {
-  final IDrawable _drawable = DrawableRect();
+  final List<IDrawable> _allDrawable = [DrawableRect(), DrawableCircle()];
+  late IDrawable _drawable;
 
   final List<Positioned> _allPositioned = [];
 
   @override
   Widget build(BuildContext context) {
+    _drawable = _allDrawable.first;
+
     return Listener(
         onPointerDown: (event) {
           setState(() {
@@ -42,7 +46,7 @@ class _DrawableZoneState extends State<DrawableZone> {
           });
         },
         child: Container(
-            color: const Color(0xFF00FF00),
+            color: const Color(0xFFEBEBEB),
             child: Stack(
               children: [
                 ..._allPositioned,
