209a2ededcea617ccc12e6c546cc75d5bd52d226
ObNitram <ObNitram@users.noreply.github.com>
2024-11-05 16:37:34 +0000

    deploy: 1fe8fdb580ad8ee28533eaad15b149c957659a5b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "ae00a66f9e2a726b09534dfc5f857cf5",
-"flutter_bootstrap.js": "962fe00580ddc1ce437415ace0672e01"};
+"flutter_bootstrap.js": "8deddb058fa6b2371077f6d210860274"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
