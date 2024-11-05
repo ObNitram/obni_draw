d3569088d927d9eccbe8b945d27fac0d33c7b25d
ObNitram <mart1.guillemot@gmail.com>
2024-11-05 18:09:42 +0100

    with git hook

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatelessWidget {
 
 
 
+
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purpleAccent),
       ),
       home: const Scaffold(body: DrawableZone()),
