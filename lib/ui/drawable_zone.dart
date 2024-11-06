a19690aa73b7c67dea6417527e5e396189c4f1ea
ObNitram <mart1.guillemot@gmail.com>
2024-11-06 15:28:15 +0100

    fix warning in DrawableZone

--- lib/ui/drawable_zone.dart
+++ lib/ui/drawable_zone.dart
@@ -7,56 +7,64 @@ import "package:obni_draw/ui/actions_bar.dart";
 import "package:obni_draw/states/actions_state.dart";
 
 class DrawableZone extends StatefulWidget {
-  DrawableZone({super.key}) {
-    _drawableTypeState = ActionsState(allDrawableType: [
-      Selector(drawableDisplayZone: _displayZoneState),
-      DrawableRectType(
-          displayZoneState: _displayZoneState,
-          backgroundColor: const Color(0x00ffffff),
-          borderColor: const Color(0xFF000000)),
-      DrawableRectType(
-          displayZoneState: _displayZoneState,
-          backgroundColor: const Color(0x2E625959),
-          borderColor: const Color(0xFF7A141B))
-    ], onChanged: () => createState());
-  }
-
-  final DisplayZoneState _displayZoneState = DisplayZoneState();
-  late ActionsState _drawableTypeState;
+  const DrawableZone({super.key});
 
   @override
   State<DrawableZone> createState() => _DrawableZoneState();
 }
 
 class _DrawableZoneState extends State<DrawableZone> {
+  final DisplayZoneState _displayZoneState = DisplayZoneState();
+  late ActionsState _drawableTypeState;
+
+  @override
+  void initState() {
+    super.initState();
+    _drawableTypeState = ActionsState(
+      allDrawableType: [
+        Selector(drawableDisplayZone: _displayZoneState),
+        DrawableRectType(
+          displayZoneState: _displayZoneState,
+          backgroundColor: const Color(0x00ffffff),
+          borderColor: const Color(0xFF000000),
+        ),
+        DrawableRectType(
+          displayZoneState: _displayZoneState,
+          backgroundColor: const Color(0x2E625959),
+          borderColor: const Color(0xFF7A141B),
+        ),
+      ],
+      onChanged: () => setState(() {}),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Listener(
         behavior: HitTestBehavior.translucent,
         onPointerDown: (event) {
-          setState(() => widget._drawableTypeState.currentDrawableType
-              .onPointerDown(event));
+          setState(() =>
+              _drawableTypeState.currentDrawableType.onPointerDown(event));
         },
         onPointerMove: (event) {
-          setState(() => widget._drawableTypeState.currentDrawableType
-              .onPointerMove(event));
+          setState(() =>
+              _drawableTypeState.currentDrawableType.onPointerMove(event));
         },
         onPointerUp: (event) {
-          setState(() =>
-              widget._drawableTypeState.currentDrawableType.onPointerUp(event));
+          setState(
+              () => _drawableTypeState.currentDrawableType.onPointerUp(event));
         },
         child: Container(
             color: const Color(0xFFEBEBEB),
             child: Stack(
               children: [
-                ...widget._displayZoneState.getPositioned(),
-                widget._drawableTypeState.currentDrawableType.draw(),
+                ..._displayZoneState.getPositioned(),
+                _drawableTypeState.currentDrawableType.draw(),
                 Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: Align(
                     alignment: Alignment.topCenter,
-                    child: ActionsBar(
-                        drawableTypeState: widget._drawableTypeState),
+                    child: ActionsBar(drawableTypeState: _drawableTypeState),
                   ),
                 )
               ],
